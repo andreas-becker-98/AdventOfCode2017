7d340685dcb9b308c5596cfb78b5f36d3c373997
Andreas Becker <andreasb.1811@gmail.com>
2017-12-12 17:54:28 +0000

    Day1 Update

---
+++ AdventOfCode2017/Day1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace AdventOfCode2017
+{
+    class Day1 : Day
+    {
+        private bool isPartTwo;
+        private string captcha;
+
+        public Day1(int part)
+        {
+            isPartTwo = part != 1;
+            captcha = File.ReadAllText(Program.path + "/Resources/day1.txt");
+        }
+
+        public override void Start()
+        {
+            isRunning = true;
+
+            int length = captcha.Length;
+            int step = isPartTwo ? length / 2 : 1;
+            int sum = 0;
+
+            for (int i = 0; i < length; ++i)
+            {
+                if (captcha[i] == captcha[(i + step) % length])
+                {
+                    sum += captcha[i] - '0';
+                }
+            }
+
+            Console.Write(string.Format("Output:\t{0}\n\n", sum));
+
+            isRunning = false;
+        }
+    }
+}
---
+++ AdventOfCode2017/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace AdventOfCode2017
+{
+    class Program
+    {
+        public static string path;
+
+        static void Main(string[] args)
+        {
+            path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            Day day;
+            string introduction = File.ReadAllText(path + "/Resources/introduction.txt");
+            string input;
+
+            Start:
+
+            Console.Clear();
+            Console.Write(introduction);
+
+            AwaitInput:
+            input = Console.ReadLine();
+
+            switch (input.ToUpper())
+            {
+                case "1.1":
+                    day = new Day1(1);
+                    break;
+                case "1.2":
+                    day = new Day1(2);
+                    break;
+
+                case "END":
+                    goto End;
+
+                default:
+                    goto Start;
+            }
+
+            day.Start();
+
+            goto AwaitInput;
+
+            End:
+            Console.Write("");
+        }
+    }
+}
